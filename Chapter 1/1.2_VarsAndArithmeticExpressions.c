b2980f60ccb4e22cb1a3226e54c23eb9472ff2f1
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-10 21:36:34 -0700

    done 1.2

---
+++ Chapter 1/1.2_VarsAndArithmeticExpressions.c
@@ -0,0 +1,33 @@
+/*
+    Celsius to Fahrenhiet
+    C = (5/9)(f-32)
+*/
+
+#include <stdio.h>
+
+/*
+    print Fahrenhiet-Celsius table
+    for fahr = 0, 20, ... , 300
+*/
+
+/*
+    %d print as decimal integer
+    %6d print as decimal integer, at least 6 chars wide
+    %f print as floating point
+    %6f print as floating point, 2 characters after decimal point
+    %6.2f print as floating point, at least 6 wide and 2 decimal points
+*/
+
+main() 
+{
+    int lower = 0, upper = 300, increment = 20;
+    int fahr = lower, celsius;
+
+    while (fahr <= upper)
+    {
+        celsius = (5 * (fahr -32)) / 9;
+        printf("%d\t%d\n", fahr, celsius); // %d is placeholder for second and third arg; \t = tab
+        fahr += increment;
+    }
+    
+}
