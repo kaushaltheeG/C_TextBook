8c04ded3920b27b6dad3af9d84119b8a064a8562
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-10 19:43:59 -0700

    First commit

---
+++ Chapter 1/1.1_GettingStarted.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // the standard input/output library | common within min C files
+
+// Main is the function where the program begins executing
+
+main() 
+{
+    printf("hello, world\n");
+
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+
+}
